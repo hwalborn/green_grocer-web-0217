4657e79139f08cf76ca8b5de61f6e8d37e25730a
Holt Walborn <hwalborn@gmail.com>
2017-01-12 23:31:36 +0000

    Done-zo

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,59 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  item_arr = []
+  return_hash = {}
+  cart.each {|value|
+    value.each{|item, info|
+      item_arr << item
+      info[:count] = item_arr.count(item)
+      return_hash[item] = info
+    }
+  }
+  return_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  return_hash = {}
+  cart.each {|item, attributes|
+    return_hash[item] = attributes
+    coupons.each{|coupon|
+      if (coupon[:item] == item) && (coupon[:num] <= return_hash[item][:count])
+        return_hash[item][:count] = attributes[:count] - coupon[:num]
+        return_hash["#{item} W/COUPON"] = {price: coupon[:cost], clearance: attributes[:clearance], count: coupons.count(coupon)}
+      elsif (coupon[:item] == item) && (coupon[:num] > return_hash[item][:count])
+        counter = 1
+        return_hash["#{item} W/COUPON"] = {price: coupon[:cost], clearance: attributes[:clearance], count: (coupons.count(coupon) - counter)}
+        counter += 1
+      end
+    }
+  }
+  return_hash
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each{|item, attribute|
+    if attribute[:clearance] == true
+      attribute[:price] = attribute[:price] - (attribute[:price] * 0.20)
+    end
+  }
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  checkout_total = 0
+  #binding.pry
+  cart = consolidate_cart(cart)
+  #binding.pry
+  cart = apply_coupons(cart, coupons)
+  #binding.pry
+  cart = apply_clearance(cart)
+  #binding.pry
+  cart.each {|item, attribute|
+    checkout_total += attribute[:price] * attribute[:count]
+  }
+  if checkout_total > 100
+    checkout_total = checkout_total - (checkout_total * 0.10)
+  end
+  checkout_total
 end
